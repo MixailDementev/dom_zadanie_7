d0b47d3c468bb12dac383c7d5903855132e589dc
MDementev <vanneste1207@gmail.com>
2022-09-11 19:17:44 +0300

    3 задача

---
+++ zadanie3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.Write("Еnter the number of rows in the array: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Еnter the number of colums in the array: ");
+int colums = int.Parse(Console.ReadLine());
+System.Console.WriteLine();
+
+
+int[,] array = EnterArray(rows, colums);
+int[,] EnterArray(int rows, int columnsint)
+{
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[i, j] = new Random().Next(10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMeanOfColumn(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum+=array[i,j];
+        }
+        Console.Write($"{ sum / array.GetLength(0)} ");
+    }
+}
+PrintArray(array);
+Console.WriteLine();
+ArithmeticMeanOfColumn(array);
